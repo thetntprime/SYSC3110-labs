4eda5b5590e92e9c542152afdad0f8218514489c
Jackie Zhuang <jackie_prime@hotmail.ca>
2021-09-17 12:11:57 -0400

    git commit

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+public class AddressBook {
+    ArrayList<BuddyInfo> buddyList = new ArrayList<BuddyInfo>();
+
+    public AddressBook(ArrayList buddyList){
+        this.buddyList = buddyList;
+    }
+
+    public void addBuddy(BuddyInfo newBuddy){
+        buddyList.add(newBuddy);
+    }
+
+    public ArrayList getBuddy(){
+        return buddyList;
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        for (int i = 0; i < buddyList.size(); i++){
+            if (buddyList.get(i).equals(buddy)){
+                buddyList.remove(i);
+            }
+            else{
+                System.out.println("The buddy does not exist in this address book");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,33 @@
+public class BuddyInfo {
+
+    private String buddyName;
+    private String buddyAddress;
+    private String buddyPhoneNumber;
+
+    public BuddyInfo(){ //default constructor
+        this("Steve","123 Pizza Street","111-222-3333");
+    }
+
+    public BuddyInfo(String buddyName, String buddyAddress, String buddyPhoneNumber) {
+        this.buddyName = buddyName;
+        this.buddyAddress = buddyAddress;
+        this.buddyPhoneNumber = buddyPhoneNumber;
+    }
+
+    public String getBuddyName() {
+        return buddyName;
+    }
+
+    public String getBuddyAddress() {
+        return buddyAddress;
+    }
+
+    public String getBuddyPhoneNumber() {
+        return buddyPhoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo BI = new BuddyInfo("John","","");
+        System.out.println("Hello "+BI.getBuddyName());
+    }
+}
